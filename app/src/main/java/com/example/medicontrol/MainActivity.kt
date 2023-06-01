c24d07120fc446b2b9e676f1944454bee4894508
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-02 00:22:09 +0300

    Added Medication model

---
+++ app/src/main/java/com/example/medicontrol/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.medicontrol
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/example/medicontrol/model/Medication.kt
@@ -0,0 +1,17 @@
+package com.example.medicontrol.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "medications")
+data class Medication(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val dosage: String,
+    val frequency: String,
+    val duration: String,
+    val purchasedCount: Int,
+    val foodDependency: String,
+    val medicationTimes: List<String> = emptyList()
+)
